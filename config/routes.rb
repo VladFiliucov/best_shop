f3ca0bc27874680afb1d3edc258086d735caabef
Vlad Filiucov <filiucov@mail.ru>
2015-08-16 19:04:11 +0100

    Add devise. Add orders. Add order_items. Display error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :products
 
   resource :cart, only: [ :show ] do
     post "add", path: "add/:id"
+    get :checkout
   end
 
-  root 'products#index'
+  resources :orders, only: [ :index, :show, :create ]
 
+  root 'products#index'
 end
